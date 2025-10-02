8dc3501d604cf41e4abb38a7fd301ff72d43aadb
nadilHesara <nadilhesara@gmail.com>
2025-10-02 12:12:05 +0530

    unnecessary cosole.logs deleted

--- backend/src/server.js
+++ backend/src/server.js
@@ -69,5 +69,5 @@ app.use((err, _req, res, _next) => {
 const PORT = Number(process.env.PORT) || 9000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`http://localhost:${PORT}/health`);
+  
 });
