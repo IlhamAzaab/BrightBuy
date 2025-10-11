afb7551a850f079cb1b3ea5683d15c9ba39d6b93
Harshana1004 <hagunawardena10@gmail.com>
2025-10-12 03:15:47 +0530

    quarterly sales report completed

--- frontend/src/components/admin/Navbar.jsx
+++ frontend/src/components/admin/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { assets } from '../../assets/assets'; // Keep your assets import
+import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
 
   return (
     <div className="flex items-center px-4 md:px-8 py-3 justify-between border-b">
@@ -13,7 +15,7 @@ const Navbar = () => {
         src={assets.logo}
         alt="Logo"
       />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm" onClick={() => {logout(); navigate("/")}}>
         Logout
       </button>
     </div>
